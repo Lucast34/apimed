b2ab8c130596768301c5c31efc5d2b2fbd134b09
Lucast34 <lucast34t@gmail.com>
2025-09-18 09:26:51 -0300

    Second commit

---
+++ api/src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -0,0 +1,17 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosListagemMedico(
+    Long id, 
+    String nome, 
+    String email,
+    String telefone, 
+    String crm, 
+    Endereco endereco,
+    Especialidade especialidade   
+){
+    public DadosListagemMedico(Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEndereco(), medico.getEspecialidade());
+    }
+}
