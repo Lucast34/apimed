e358f2312ef618cf9bea4c7356887c9553703ae7
Lucast34 <lucast34t@gmail.com>
2025-09-23 11:35:47 -0300

    Update api && Create consulta

--- api/src/main/resources/db/migration/V1__create-table-paciente.sql
+++ api/src/main/resources/db/migration/V1__create-table-paciente.sql
@@ -2,5 +2,13 @@ CREATE TABLE IF NOT EXISTS tb_paciente(
     id bigint primary key not null auto_increment,
     nome varchar(100) not null,
     email varchar(100) not null unique,
-    telefone varchar(100) not null
+    telefone varchar(100) not null,
+    logradouro varchar(100) not null,
+    bairro varchar(100) not null,
+    cep varchar(9) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    uf char(2) not null,
+    cidade varchar(100) not null
+    
 );
